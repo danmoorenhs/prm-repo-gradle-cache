927939108745bc16f7a8b74b9b2a12538b5c7b4b
Dan Moore <dan.moore1@nhs.net>
2021-12-22 20:29:12 +0000

    [TECH] Explicitly clean and recreate cache dir

--- _build_cache.sh
+++ _build_cache.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-time gradle dependencies
-cp -R ~/.gradle/caches/modules-2/ .gradle-readonly-cache/
+TARGET_CACHE_DIR=.gradle-readonly-cache/
 
-pushd .gradle-readonly-cache
+echo removing existing cache dir
+rm -rf $TARGET_CACHE_DIR
+
+echo rebuilding cache dir
+mkdir $TARGET_CACHE_DIR
+time gradle dependencies
+cp -R ~/.gradle/caches/modules-2/ $TARGET_CACHE_DIR
+
+pushd $TARGET_CACHE_DIR
+echo cleaning lockfiles and gc.properties
 find . -name '*lock' -delete
-find . -name 'gs.properties' -delete
+find . -name 'gc.properties' -delete
 popd
+
+du -h $TARGET_CACHE_DIR
